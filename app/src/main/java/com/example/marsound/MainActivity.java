7edbf800ce41a1de95039ebece1c4dd15cf59bc2
Mars <201005642@lbschools.net>
2023-05-24 17:19:06 -0700

    sleepy

--- app/src/main/java/com/example/marsound/MainActivity.java
+++ app/src/main/java/com/example/marsound/MainActivity.java
@@ -2,16 +2,41 @@ package com.example.marsound;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     //Buttons to hit
 
-    private Button btnBass
+    private Button btnQuandale;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnQuandale = findViewById(R.id.btn_quandale);
+
+        btnQuandale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playQuandale();
+            }
+        });
+    }
+
+
+    private void playQuandale(){
+        MediaPlayer mp = MediaPlayer.create(this,R.raw.sound_quandale);
+        mp.start();
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.release();
+                mp = null;
+            }
+        });
     }
 }
